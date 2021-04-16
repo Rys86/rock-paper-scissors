e1bf1cdeef4379a369bf1498eea7882035b6b03f
Rys86 <turkowski.mariusz@gmail.com>
2021-04-17 00:11:15 +0200

    some test code & --> basic algorythm created

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
 console.log("Hello World!");
 let promption = window.prompt("Please, type in some random text.","Very random text.");
 window.alert(promption);
+/*Computer is asking passing the greetings and asking for the first choice
+// -> function: userInput
+// | computer is making the request for next choice 
+// | program is validating users imput
+// | if users imput is invalid, computer is asking for imput once again
+// -> function: computerPlay
+// |Computer is drawing a lots 
+// -> function: playRound:
+// | program is comparing his choice with users choice
+// | program is giving the feedback who wins the round 
+// - > Function: game
+// | Program is playing 5 rounds
+// | program is sumarizing the score
+*/
