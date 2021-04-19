138c90babc67bf0f07dee4128f9f7f8f2cef3dfa
Rys86 <turkowski.mariusz@gmail.com>
2021-04-19 23:59:53 +0200

    | index.html -> page tittle added | main.js -> promptValidation function Created & user input function frame added

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,28 @@
 console.log("Hello World!");
 let promption = window.prompt("Please, type in some random text.","Very random text.");
 window.alert(promption);
-/*Computer is asking passing the greetings and asking for the first choice
+//Computer is asking passing the greetings and asking for the first choice
 // -> function: userInput
-// | computer is making the request for next choice 
-// | program is validating users imput
-// | if users imput is invalid, computer is asking for imput once again
+function userInput(userChoice) {
+  if (userChoice) {
+    // | computer is making the request for next choice
+    let promption = window.prompt("Please, type in some random text.","");
+
+  } else {
+    //Computer is asking passing the greetings and asking for the first choice
+  }
+}
+function promptValidation(userChoice) { 
+  // | program is validating users imput
+  if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
+    return userChoice;
+  } else { 
+    // | if users imput is invalid, computer is asking for imput once again
+    window.alert(`Incorrect input value: ${userChoice}, please, type one of those: rock, paper or scissors`)
+    return userInput(userChoice);
+  }
+}
+
 // -> function: computerPlay
 // |Computer is drawing a lots 
 // -> function: playRound:
@@ -14,4 +31,4 @@ window.alert(promption);
 // - > Function: game
 // | Program is playing 5 rounds
 // | program is sumarizing the score
-*/
+//
